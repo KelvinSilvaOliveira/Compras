92e512c8ec46490337913ce80ffd0e9810f37dda
KelvinSilvaOliveira <86942180+KelvinSilvaOliveira@users.noreply.github.com>
2022-08-18 00:36:08 -0300

    feat: initial configs of project spring (#1)

---
+++ src/main/java/com/listadecompras/compras/ComprasApplication.java
@@ -0,0 +1,13 @@
+package com.listadecompras.compras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComprasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComprasApplication.class, args);
+	}
+
+}
